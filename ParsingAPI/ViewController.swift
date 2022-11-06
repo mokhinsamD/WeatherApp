af57758cd9407bf819066afaf7c342de5090500d
Darina Samokhina <mokhinsam@yandex.ru>
2022-11-06 12:47:33 +0300

    Initial Commit

---
+++ ParsingAPI/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ParsingAPI
+//
+//  Created by Дарина Самохина on 06.11.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
