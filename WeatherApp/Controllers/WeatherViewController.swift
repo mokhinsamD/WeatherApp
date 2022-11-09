3cb379098fa2cc94e435f336bd51e26c685974d6
Darina Samokhina <mokhinsam@yandex.ru>
2022-11-10 00:38:27 +0300

    added network manager logic

--- WeatherApp/Controllers/WeatherViewController.swift
+++ WeatherApp/Controllers/WeatherViewController.swift
@@ -8,7 +8,7 @@
 import UIKit
 
 class WeatherViewController: UIViewController {
-
+    
     @IBOutlet var cityNameLabel: UILabel!
     @IBOutlet var imageView: UIImageView!
     @IBOutlet var degreeLabel: UILabel!
@@ -19,49 +19,30 @@ class WeatherViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         fetchWeather()
-        fetchImage()
     }
-    
-    private func fetchWeather() {
-        guard let url = URL(string: "https://api.weatherapi.com/v1/current.json?key=d1b6e7d23c7d433eb1a95416220611&q=moscow") else {
-            return
-        }
-        
-        URLSession.shared.dataTask(with: url) { data, _, error in
-            guard let data = data else {
-                print(error?.localizedDescription ?? "No error description")
-                return
-            }
-            
-            let decoder = JSONDecoder()
-            decoder.keyDecodingStrategy = .convertFromSnakeCase
-            do {
-                let weather = try decoder.decode(Weather.self, from: data)
-                DispatchQueue.main.async { [unowned self] in
-                    cityNameLabel.text = weather.location.name
-                    degreeLabel.text = String(weather.current.tempC)
-                    spedWindLabel.text = String(weather.current.windMph)
-                    imageView.image = UIImage(named: "https://\(weather.current.condition.icon)")
-                }
-                print(weather)
-            } catch {
-                print(error.localizedDescription)
-            }
-        }.resume()
-    }
-
-    private func fetchImage() {
-        NetworkManager.shared.fetchImage(from: "https://cdn.weatherapi.com/weather/64x64/day/296.png") { [unowned self] result in
-            switch result {
-            case .success(let imageData):
-                imageView.image = UIImage(data: imageData)
-            case .failure(let error):
-                print(error)
-            }
-        }
-    }
-    
-    
     
 }
 
+extension WeatherViewController {
+    private func fetchWeather() {
+        NetworkManager.shared.fetchWeather(from: url) { [unowned self] result in
+            switch result {
+            case .success(let weather):
+                cityNameLabel.text = weather.location.name
+                degreeLabel.text = String(weather.current.tempC)
+                spedWindLabel.text = String(weather.current.windMph)
+                guard let imageURL = URL(string: "https:\(weather.current.condition.icon)") else { return }
+                DispatchQueue.global().async {
+                    guard let imageData = try? Data(contentsOf: imageURL) else {
+                        return
+                    }
+                    DispatchQueue.main.async { [unowned self] in
+                        imageView.image = UIImage(data: imageData)
+                    }
+                }
+            case .failure(let error):
+                print(error.localizedDescription)
+            }
+        }
+    }
+}
--- WeatherApp/Services/NetworkManager.swift
+++ WeatherApp/Services/NetworkManager.swift
@@ -19,52 +19,30 @@ class NetworkManager {
     
     private init() {}
     
-    func fetchImage(from url: String, completion: @escaping(Result<Data, NetworkError>) -> Void) {
+    func fetchWeather(from url: String, completion: @escaping(Result<Weather, NetworkError>) -> Void) {
         guard let url = URL(string: url) else {
             completion(.failure(.invalidURL))
             return
         }
-        DispatchQueue.global().async {
-            guard let imageData = try? Data(contentsOf: url) else {
+        
+        URLSession.shared.dataTask(with: url) { data, _, error in
+            guard let data = data else {
                 completion(.failure(.noData))
+                print(error?.localizedDescription ?? "No error description")
                 return
             }
             
-            DispatchQueue.main.async {
-                completion(.success(imageData))
+            let decoder = JSONDecoder()
+            decoder.keyDecodingStrategy = .convertFromSnakeCase
+            do {
+                let weather = try decoder.decode(Weather.self, from: data)
+                DispatchQueue.main.async {
+                    completion(.success(weather))
+                }
+            } catch {
+                print(error.localizedDescription)
             }
-            
-        }
+        }.resume()
     }
-    
 }
-    
-//    func fetchWeather() {
-//        guard let url = URL(string: "https://api.weatherapi.com/v1/current.json?key=d1b6e7d23c7d433eb1a95416220611&q=moscow") else {
-//            return
-//        }
-//
-//        URLSession.shared.dataTask(with: url) { data, _, error in
-//            guard let data = data else {
-//                print(error?.localizedDescription ?? "No error description")
-//                return
-//            }
-//
-//            let decoder = JSONDecoder()
-//            decoder.keyDecodingStrategy = .convertFromSnakeCase
-//            do {
-//                let weather = try decoder.decode(Weather.self, from: data)
-//                DispatchQueue.main.async { [unowned self] in
-//                    cityNameLabel.text = weather.location.name
-//                    degreeLabel.text = String(weather.current.tempC)
-//                    spedWindLabel.text = String(weather.current.windMph)
-//                    imageView.image = UIImage(named: "https://\(weather.current.condition.icon)")
-//                }
-//                print(weather)
-//            } catch {
-//                print(error.localizedDescription)
-//            }
-//        }.resume()
-//    }
-//}
 
